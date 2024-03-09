695c6bbbe51341056c9c15ee6c1d94a1bf05d78b
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2024-03-10 00:03:07 +0300

    Fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 const navLinks = document.querySelector(".nav-links");
 const onToggleMenu = (e) => {
   e.name = e.name === "menu-outline" ? "close-outline" : "menu-outline";
-  navLinks.classList.toggle("top-[9%]");
+  navLinks.classList.toggle("top-[8%]");
 };
