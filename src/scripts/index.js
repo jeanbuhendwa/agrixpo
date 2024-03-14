06eade58be0b235fb0ad58c4ae0fffbc48419d89
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2024-03-15 00:59:32 +0300

    Update files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Toggle dropdown
   dropdownButton.addEventListener("click", function (event) {
     dropdownMenu.classList.toggle("hidden");
-    event.stopPropagation(); // Prevent the click from being detected by the document listener
+    event.stopPropagation();
   });
 
   // Hide the menu when clicking outside
@@ -57,5 +57,81 @@ roleButtons.forEach((button) => {
         btn.classList.add("bg-white", "text-[#384E46]");
       }
     });
+  });
+});
+
+// Function for the categories
+const categories = [
+  {
+    name: "Fruits",
+    image: "https://source.unsplash.com/featured/?fruits",
+    link: "/fruits.html",
+  },
+  {
+    name: "Dairy",
+    image: "https://source.unsplash.com/featured/?milk",
+    link: "/dairy.html",
+  },
+  {
+    name: "Vegetables",
+    image: "https://source.unsplash.com/featured/?vegetables",
+    link: "/vegetables.html",
+  },
+  {
+    name: "Legumes",
+    image: "https://source.unsplash.com/featured/?legumes",
+    link: "/legumes.html",
+  },
+  {
+    name: "Meat",
+    image: "https://source.unsplash.com/featured/?meat",
+    link: "/meat.html",
+  },
+  {
+    name: "Poultry",
+    image: "https://source.unsplash.com/featured/?poultry",
+    link: "/poultry.html",
+  },
+  {
+    name: "Fish",
+    image: "https://source.unsplash.com/featured/?fish",
+    link: "/fish.html",
+  },
+  {
+    name: "Seafood",
+    image: "https://source.unsplash.com/featured/?seafood",
+    link: "/seafood.html",
+  },
+];
+
+const initialDisplayCount = 8;
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const grid = document.getElementById("productGrid");
+  grid.className = "grid grid-cols-1 md:grid-cols-5 gap-4 md:mx-10 mx-5";
+
+  categories.forEach((category, index) => {
+    const card = document.createElement("div");
+    card.className = `max-full bg-white rounded overflow-hidden shadow-lg cursor-pointer transform transition duration-500 hover:scale-105 ${
+      index >= initialDisplayCount ? "hidden" : ""
+    } category-card`;
+    card.innerHTML = `
+      <img class="w-full h-[160px]" src="${category.image}" alt="${category.name}">
+      <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">${category.name}</div>
+      </div>
+    `;
+    card.addEventListener("click", () => {
+      window.location.href = category.link;
+    });
+    grid.appendChild(card);
+  });
+
+  const showMoreBtn = document.getElementById("showMoreBtn");
+  showMoreBtn.addEventListener("click", () => {
+    document.querySelectorAll(".category-card.hidden").forEach((card) => {
+      card.classList.remove("hidden");
+    });
+    showMoreBtn.classList.add("hidden"); // Hide the button after expanding
   });
 });
